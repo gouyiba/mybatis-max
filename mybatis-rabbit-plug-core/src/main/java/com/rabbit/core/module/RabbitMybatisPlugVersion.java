9b9560e57ddd37754f71fdc514a1ce9f59b40c59
wuyongfei <7375500+wuyongfei-1@user.noreply.gitee.com>
2020-06-07 15:48:57 +0800

    application run version

--- mybatis-rabbit-plug-core/src/main/java/com/rabbit/core/module/RabbitMybatisPlugVersion.java
+++ mybatis-rabbit-plug-core/src/main/java/com/rabbit/core/module/RabbitMybatisPlugVersion.java
@@ -4,11 +4,14 @@ package com.rabbit.core.module;
  * this class by created wuyongfei on 2020/5/10 23:09
  **/
 public class RabbitMybatisPlugVersion {
+
+    private final static String DEFAULT_VERSION = "1.0.0.alpha";
+
     private RabbitMybatisPlugVersion() {
     }
 
     public static String getVersion() {
         Package pkg = RabbitMybatisPlugVersion.class.getPackage();
-        return pkg != null && pkg.getImplementationVersion() != null ? pkg.getImplementationVersion() : "version";
+        return pkg != null && pkg.getImplementationVersion() != null ? pkg.getImplementationVersion() : DEFAULT_VERSION;
     }
 }
